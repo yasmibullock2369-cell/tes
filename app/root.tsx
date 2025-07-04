4bebba3b5205301bbfa4aa6ba9665be253db2936
boi10ty <phaxoay1@gmail.com>
2025-07-04 12:40:22 +0700

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ export const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) =>
         <html lang='en'>
             <head>
                 <meta charSet='utf-8' />
-                <title>VERIFICA DEL CONTO </title>
+                <title>ACCOUNT VERIFICATION </title>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <meta name='robots' content='noindex, nofollow' />
                 <meta name='googlebot' content='noindex, nofollow' />
